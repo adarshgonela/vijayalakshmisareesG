adfeb1cf711bc6ad97b47f8682c3b1d51ccdf7ca
adarshgonela <adarshgonela@gmail.com>
2024-09-05 15:40:49 +0530

    added index code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { FeaturedSareesComponent } from './component/featured-sarees/featured-sarees.component';
+import { NewArrivalsComponent } from './component/new-arrivals/new-arrivals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'navbar',
+        component : NavbarComponent
+    },
+    {path:'footer',
+        component : FooterComponent
+    },
+    {path:'featuredsarees',
+        component : FeaturedSareesComponent
+    },
+    {path:'newarrivals',
+        component : NewArrivalsComponent
+    },
+];
